915c83bcd8e15cdda447f97167bf7842d8ba7e80
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2024-10-24 13:11:46 -0300

    ok

--- book-club/data/book-club.js
+++ book-club/data/book-club.js
@@ -1,18 +1,42 @@
 function toggleMenu() {
-    let menu = document.querySelector('#hamburguer');
-    menu.classList.toggle('open');
+    let menu = document.querySelector('.flex-nav ul');
+    menu.classList.toggle('show');
 
-    let hamburgerButton = document.querySelector('.mobile-menu-icon');
+    let hamburgerButton = document.querySelector('.hamburger');
     if (hamburgerButton) {
-        if (menu.classList.contains('open')) {
+        if (menu.classList.contains('show')) {
             hamburgerButton.textContent = 'X';
         } else {
             hamburgerButton.textContent = '☰';
         }
     }
 }
+document.addEventListener('DOMContentLoaded', function () {
+    let links = document.querySelectorAll('.navigation ul');
+    links.forEach(link => {
+        if (window.location.href.includes(link.getAttribute('href'))) {
+            link.classList.add('active');
+        }
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    let yearSpan = document.getElementById('currentyear');
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
 
-let hamburgerButton = document.querySelector('.mobile-menu-icon');
-if (hamburgerButton) {
-    hamburgerButton.addEventListener('click', toggleMenu);
-}
+    let lastModifiedSpan = document.getElementById('last-modified');
+    if (lastModifiedSpan) {
+        const now = new Date();
+        lastModifiedSpan.textContent = now.toLocaleString();
+    }
+
+    const currentPath = window.location.pathname;
+    const menuItems = document.querySelectorAll('nav ul li a');
+
+    menuItems.forEach(item => {
+        if (item.getAttribute('href') === currentPath) {
+            item.classList.add('active');
+        }
+    });
+});
